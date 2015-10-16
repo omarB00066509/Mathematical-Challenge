89f401990c8f93733aefaa1cf4289cdd8b8a277f
Omar Ibrahim <omar21cu@yahoo.es>
2015-10-16 19:40:12 +0100

    Scripts non functional!
    Added Scripts we need to use, modified and organized location of files
    in appropriate folders. Added Wall limits around the maps on scenes.

---
+++ Assets/Scripts/DestroyWhenHit.cs
@@ -0,0 +1,11 @@
+﻿/** \Author Matt Smith */
+using UnityEngine;
+using System.Collections;
+
+public class DestroyWhenHit : MonoBehaviour {
+
+	/** destroy object */
+	private void OnTriggerEnter(){
+		Destroy(this.gameObject);
+	}
+}
