cde683e224b84280d39818c510b3d9df7cc5dcc0
Omar Ibrahim <omar21cu@yahoo.es>
2015-10-17 16:50:02 +0100

    Randomize position Script
    Randomize position script is added

--- Assets/Scripts/RandomizePositions.cs
+++ Assets/Scripts/RandomizePositions.cs
@@ -3,12 +3,24 @@ using System.Collections;
 
 public class RandomizePositions : MonoBehaviour {
 
-	public GameObject numbers, operators;
+	public GameObject Spawns;
 
 	// Use this for initialization
 	void Start () {
+
+		Transform[] spawnedObjects = Spawns.GetComponentsInChildren<Transform>();
+
+		foreach (Transform child in spawnedObjects) {
+			float Yposition = child.gameObject.transform.position.y;
+			Vector3 rndPosition = new Vector3(Random.Range(0,20), Yposition, Random.Range(0,20));
+			
+			//if (child.gameObject.tag.Equals("Number")){
+				child.gameObject.transform.position = rndPosition;
+			//}
+		}
+		/**
 		numbers = GameObject.FindGameObjectWithTag ("Number");
-		operators = GameObject.FindGameObjectWithTag ("Operator");
+		operators = GameObject.FindGameObjectWithTag ("X");
 
 		Transform[] childNumber = numbers.GetComponentsInChildren<Transform>();
 		Transform[] childOperator = operators.GetComponentsInChildren<Transform>();
@@ -29,7 +41,7 @@ public class RandomizePositions : MonoBehaviour {
 				Vector3 rndPosition = new Vector3(Random.Range(-45,45), Yposition, Random.Range(-45,45));
 				child.gameObject.transform.position = rndPosition;
 			}
-		}
+		} */
 	}
 
 }
