16d809d1187128754d71e4aee13d390ed04742b6
Omar Ibrahim <omar21cu@yahoo.es>
2015-10-24 14:42:59 +0100

    Randomize spawns values
    Added the possibility of instantiating randomized values to the spawns
    objects (numbers, operators)

--- Assets/Scripts/InstantiateObjects.cs
+++ Assets/Scripts/InstantiateObjects.cs
@@ -21,15 +21,19 @@ public class InstantiateObjects : MonoBehaviour {
 			// generate random position in a Vector3
 			float Yposition = child.gameObject.transform.position.y;
 			Vector3 rndPosition = new Vector3(Random.Range(10,30), Yposition, Random.Range(10,30));
+			string[] operatorsStr = {"+","-","x", "+", "+", "-","x","-"};
+
 
 			if (child.name.Equals("Number")) {
 				//instantiate the object and assigne a random position
 				Instantiate(numberObject).gameObject.transform.position = rndPosition;
+				numberObject.GetComponent<TextMesh>().text = Random.Range(1,10).ToString();
 
 			}
 			if(child.name.Equals("Operator")){
 				//instantiate the object and assigne a random position
 				Instantiate(operatorObject).gameObject.transform.position = rndPosition;
+				operatorObject.GetComponent<TextMesh>().text = operatorsStr[Random.Range(0,operatorsStr.Length)];
 
 			}
 		}
